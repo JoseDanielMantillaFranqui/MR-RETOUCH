a304bfb732238299c6428b32f0c9557cd6413c2a
José Daniel Mantilla Franqui <danielfranqui016@gmail.com>
2024-07-10 11:22:11 -0500

    Añadida página 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Home from './screens/Home'
 import Response from './screens/Response'
+import Page404 from './screens/Page404'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/response' element={<Response />} />
+      <Route path='*' element={ <Page404 />} />
     </Routes>
   </Router>
 }
---
+++ src/screens/Page404.jsx
@@ -0,0 +1,14 @@
+const Page404 = () => {
+    return <section className='main__container' style={{ backgroundImage: 'url(https://i.gifer.com/76cE.gif)' }}>
+    <div className='interface'>
+        <div className='interface__card--404'>
+            <h1 className='card__title--404'>
+                ERROR 404
+                <p>RECURSO NO ENCONTRADO</p>
+            </h1>
+        </div>
+    </div>
+</section>
+}
+
+export default Page404
